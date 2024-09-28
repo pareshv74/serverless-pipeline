1ed232e8890da86ce0c7b07e7ca695277e02b65a
pareshv74 <varankarparesh74@gmail.com>
2024-09-28 23:38:34 +0530

    Update codebuild.tf

--- modules/pipeline/codebuild.tf
+++ modules/pipeline/codebuild.tf
@@ -1,7 +1,7 @@
 data "template_file" "buildspec" {
   template = "${file("${path.module}/templates/buildspec.yml")}"
 
-  vars {
+  vars = {
     app_name   = "${var.app_name}"
   }
 }
@@ -12,7 +12,7 @@ resource "aws_codebuild_project" "app_build" {
 
   service_role = "${aws_iam_role.codebuild_role.arn}"
 
-  artifacts {
+  artifacts = {
     type = "CODEPIPELINE"
   }
 
@@ -25,7 +25,7 @@ resource "aws_codebuild_project" "app_build" {
     privileged_mode = true
   }
 
-  source {
+  source = {
     type      = "CODEPIPELINE"
     buildspec = "${data.template_file.buildspec.rendered}"
   }
